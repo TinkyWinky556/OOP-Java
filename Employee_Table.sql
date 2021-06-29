44bb31f0aed61d19383093391ff2c308a2d172f0
edward <62472600+edward556@users.noreply.github.com>
2021-06-29 21:28:04 +0700

    update db

--- Employee_Table.sql
+++ Employee_Table.sql
@@ -19,7 +19,7 @@ USE COMPANY;
 
 -- SQL Table Syntax For This Project.
 -- Table_Name = "EMPLOYEE"
-CREATE TABLE EMPLOYEE(Employee_ID VARCHAR(255), Name VARCHAR(255), Gender VARCHAR(255), Address VARCHAR(255), Mobile VARCHAR(255), Date_Employed VARCHAR(255), Designation VARCHAR(255), Monthly_Payment_Rate VARCHAR(255));
+CREATE TABLE EMPLOYEE(Employee_ID VARCHAR(255) NOT NULL, Name VARCHAR(255) NOT NULL, Gender VARCHAR(255) NOT NULL, Address VARCHAR(255) NOT NULL, Mobile VARCHAR(255) NOT NULL, Date_Employed VARCHAR(255) NOT NULL, Designation VARCHAR(255) NOT NULL, Monthly_Payment_Rate VARCHAR(255) NOT NULL);
 
 -- Test The Response Of SQL Table With These Insert & Select Queries.
 INSERT INTO EMPLOYEE VALUES('53409','Dora The Explorer','Female','Peruvian Jungle','UNKNOWN','25/01/2021','The Explorer','US$ 11,933.00');
